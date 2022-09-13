8a795d9ccd9c5ee11c03894c8b8e95e9f0235b36
Peggy Liang <63401571+PL42@users.noreply.github.com>
2022-09-13 12:26:19 +0800

    Update deps

---
+++ src/sentry/sentry_client.rs
@@ -0,0 +1,50 @@
+use super::{
+    chain_config::ChainConfig,
+    messages::{EthMessageId, Message},
+};
+use async_trait::async_trait;
+use futures_core::Stream;
+use std::{fmt::Debug, pin::Pin};
+
+pub struct Status {
+    pub total_difficulty: ethereum_types::U256,
+    pub best_hash: ethereum_types::H256,
+    pub chain_fork_config: ChainConfig,
+    pub max_block: u64,
+}
+
+#[derive(Clone, Debug)]
+pub enum PeerFilter {
+    MinBlock(u64),
+    PeerId(ethereum_types::H512),
+    Random(u64 /* max peers */),
+    All,
+}
+
+#[derive(Clone, Debug)]
+pub struct MessageFromPeer {
+    pub message: Message,
+    pub from_peer_id: Option<ethereum_types::H512>,
+}
+
+pub type MessageFromPeerStream =
+    Pin<Box<dyn Stream<Item = anyhow::Result<MessageFromPeer>> + Send>>;
+
+#[async_trait]
+pub trait SentryClient: Send + Debug {
+    async fn set_status(&mut self, status: Status) -> anyhow::Result<()>;
+
+    //async fn penalize_peer(&mut self) -> anyhow::Result<()>;
+    //async fn peer_min_block(&mut self) -> anyhow::Result<()>;
+
+    async fn send_message(
+        &mut self,
+        message: Message,
+        peer_filter: PeerFilter,
+    ) -> anyhow::Result<u32>;
+
+    async fn receive_messages(
+        &mut self,
+        filter_ids: &[EthMessageId],
+    ) -> anyhow::Result<MessageFromPeerStream>;
+}
---
+++ src/sentry/sentry_client_mock.rs
@@ -0,0 +1,72 @@
+use super::{
+    messages::{EthMessageId, Message},
+    sentry_client::{MessageFromPeer, MessageFromPeerStream, PeerFilter, SentryClient, Status},
+};
+use std::collections::HashSet;
+use tokio::sync::broadcast;
+use tokio_stream::{wrappers, StreamExt};
+
+#[derive(Debug)]
+pub struct SentryClientMock {
+    message_sender: Option<broadcast::Sender<MessageFromPeer>>,
+    message_receiver: Option<broadcast::Receiver<MessageFromPeer>>,
+}
+
+impl SentryClientMock {
+    pub fn new() -> Self {
+        let (message_sender, message_receiver) = broadcast::channel(10);
+        SentryClientMock {
+            message_sender: Some(message_sender),
+            message_receiver: Some(message_receiver),
+        }
+    }
+
+    fn stop_receiving_messages(&mut self) {
+        self.message_sender = None;
+    }
+}
+
+#[async_trait::async_trait]
+impl SentryClient for SentryClientMock {
+    async fn set_status(&mut self, _status: Status) -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    async fn send_message(
+        &mut self,
+        _message: Message,
+        _peer_filter: PeerFilter,
+    ) -> anyhow::Result<u32> {
+        self.stop_receiving_messages();
+        Ok(1)
+    }
+
+    async fn receive_messages(
+        &mut self,
+        filter_ids: &[EthMessageId],
+    ) -> anyhow::Result<MessageFromPeerStream> {
+        let filter_ids_set = filter_ids
+            .iter()
+            .cloned()
+            .collect::<HashSet<EthMessageId>>();
+
+        if let Some(receiver) = self.message_receiver.take() {
+            let stream = wrappers::BroadcastStream::new(receiver)
+                .filter_map(|res| res.ok()) // ignore BroadcastStreamRecvError
+                .filter(move |message_from_peer| {
+                    filter_ids_set.contains(&message_from_peer.message.eth_id())
+                })
+                .map(Ok);
+
+            Ok(Box::pin(stream))
+        } else {
+            anyhow::bail!("SentryClientMock::receive_messages supports only one receiver")
+        }
+    }
+}
+
+impl Default for SentryClientMock {
+    fn default() -> Self {
+        Self::new()
+    }
+}
